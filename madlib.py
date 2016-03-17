69a31b9a3081585ff02aaf32e0fa649927d7c42c
Matt Voelpel <matvop@gmail.com>
2016-03-17 10:36:42 -0700

    madlib program made on day 1

---
+++ madlib.py
@@ -0,0 +1,37 @@
+#madlib program created by Matt Voelpel 3.16.2016
+print('')
+print('Please enter an adjective.')
+adj = input()
+
+print('')
+print('Please enter a noun.')
+noun = input()
+
+print('')
+print('Please choose a type of food.')
+food = input()
+
+print('')
+print('Please enter the name of someone in the room.')
+name = input()
+
+print('')
+print('Please enter another adjective.')
+adj2 = input()
+
+print('')
+print('Please select one more noun.')
+noun2 = input()
+
+print('')
+print('Please enter the name of a city.')
+city = input()
+
+print('')
+print('Please enter your favorite condiment.')
+condiment = input()
+
+print('')
+print('There are many ' + adj + ' ways to choose a ' + noun + " to eat. First, you could ask for recommendations from your friends and family. Just don't ask Uncle " + name + '-he only eats ' + adj2 + ' ' + food + ' with ' + condiment + ' and fish oil sauce. If your friends and family are no help, try checking out the ' + noun2 + ' Review in The ' + city + ' Times.' )
+print('')
+print('')
