8ccc20e99b0acfd4ef85e066b66ff947c9344f66
Matt Voelpel <matvop@gmail.com>
2016-03-23 11:00:06 -0700

    hangman updated with graphics

--- hangman.py
+++ hangman.py
@@ -27,11 +27,108 @@ mug_of_beer = '''
        \   \   \   /   /   /
         `"""""""""""""""""`
 '''
+hang_a_man = ("""
+-----
+|   |
+|
+|
+|
+|
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+|
+|
+|
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+|  -+-
+|
+|
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-
+|
+|
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-\\
+|
+|
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-\\
+|   |
+|   |
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-\\
+|   |
+|   |
+|  |
+|  |
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-\\
+|   |
+|   |
+|  | |
+|  | |
+|
+--------
+""")
+
 def random_word_list():
     """Selects a word at random from words and returns as random_word"""
     words = ['CODING', 'SOFTWARE', 'PYTHON', 'JAVASCRIPT', 'ENGINEER', 'DEVELOPER', 'PROGRAMMING', 'FUNCTIONS', 'DJANGO', 'RUBY', 'LITERALS', 'ITERABLES', 'OPERATORS', 'BOOLEAN', 'INTEGER', 'STRING', 'FLOAT', 'LOOP']
-    random_word = words[random.randint(0,len(words))]# - my original method of selecting a random word
-    #random_word = random.choice(words) #more efficient method of selecing a random word
+    #random_word = words[random.randint(0,len(words)-1)]# - my original method of selecting a random word
+    random_word = random.choice(words) #more efficient method of selecing a random word
     return random_word
 
 def blanks_and_correct_letters(secret_word, correct_letters):
@@ -43,9 +140,11 @@ def blanks_and_correct_letters(secret_word, correct_letters):
         else:
             blanks_and_letters += '_ '
     print(blanks_and_letters)
+    print('')
 
 def guess_a_letter(secret_word, correct_letters, missed_letters):
     """Prompts them to guess a letter. Mistakes are tallied for incorrect guesses"""
+    print('')
     letter_guess = input('Please choose a letter: ').upper()
     print('')
     if letter_guess in secret_word:
@@ -69,12 +168,12 @@ def correct_word(secret_word, correct_letters):
 #     play = input('Would you like to play again? [y/n]: ')
 #     return play
 
-print('')
-os.system('cls')
 secret_word = random_word_list() #chooses the secret word from the random word list
-print("It's time to play Hangman.")
 print('')
-while mistakes_made < 7: #game loop
+while mistakes_made < (len(hang_a_man)): #game loop
+    os.system('cls')
+    print("Let's play Hangman!")
+    print(hang_a_man[mistakes_made])
     if secret_word == correct_word(secret_word, correct_letters):
         print('Congratulations, you guessed ' + secret_word + ' correctly! Here, have a beer!')
         print(mug_of_beer)
@@ -82,6 +181,7 @@ while mistakes_made < 7: #game loop
     blanks_and_correct_letters(secret_word, correct_letters)
     print(', '.join(missed_letters))
     guess_a_letter(secret_word, correct_letters, missed_letters)
+    #could insert a function that prints out a visual depiction of a hangman here
 else:
     print('You did not guess the word {}. Better luck next time!'.format(secret_word))
 print('')
