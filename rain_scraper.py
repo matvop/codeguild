81283245331031e0e094e2461bfc4318241e3ac9
Matt Voelpel <matvop@gmail.com>
2016-04-02 13:46:37 -0700

    cleaned up code - updated

--- rain_scraper.py
+++ rain_scraper.py
@@ -1,16 +1,22 @@
 ###City of Portland - Hydra Network Rain Gauge scraper
-###Retrieves hourly updated rain gauge data from or.water.usgs.gov(internet connection required)
-###Supports user selection of a specific rain gauge station
-###Currently outputs the street address of the selected location, date of most rainfall and amount of rain, location's wettest year and amount of rain
-###future revisions may include ability to notify user if location has been retired, and that data is for historic purposes
+###Retrieves hourly updated rain gauge data from or.water.usgs.gov.
+###(internet connection required)
+###Supports user selection of a specific rain gauge station.
+###Currently outputs the street address selected location,
+###date of most rainfall - amount of rain, and the location's wettest year
+###and amount of rain for that year
+###future revisions may include ability to notify user if
+###location has been retired, and that data is for historic purposes
 
 import urllib.request
 import os
 
 def parse_index_url_into_lines():
-    """Parses the main Oregon usgs rain station index webpage into a list of lines"""
-    with urllib.request.urlopen('http://or.water.usgs.gov/non-usgs/bes/') as data_file:
-        all_gauges_line_data = [byte_line.decode('utf-8') for byte_line in data_file]
+    """Parses Oregon usgs rain station index page into a list of lines"""
+    with urllib.request.urlopen(
+        'http://or.water.usgs.gov/non-usgs/bes/') as data_file:
+        all_gauges_line_data = [byte_line.decode(
+            'utf-8') for byte_line in data_file]
     return all_gauges_line_data
 
 def parse_rain_file_url_into_lines():
@@ -20,26 +26,32 @@ def parse_rain_file_url_into_lines():
     return rain_line_data
 
 def get_url():
-    """retrives the .rain file url by using the users station_num input in select_rain_gauge()"""
+    """retrives the .rain file url by using the users station_num
+    input in select_rain_gauge()"""
     url = create_rain_gauge_dict()[select_rain_gauge()][1]
     return url
 
 def select_rain_gauge():
-    """Provides the user with a table of available rain stations by their id# and location, and .rain file url. Prompts the user for station id# and returns response."""
+    """Provides the user with a table of available rain stations by their id#,
+    location, and .rain file url. Prompts user for station id#"""
     print('\n{:<15} {:<40} {:<40}'.format('Station id#:','Location:','URL:\n'))
     for k, v in create_rain_gauge_dict().items():
         location, url = v
         print('{:<15} {:<40} {:<40}'.format(k, location, url))
     print('')
-    return input("""Rain level data is available for the listed locations. Please enter station id#:
+    return input("""Rain level data is available for the listed locations.
 
-    > """)
+Please enter station id#: """)
 
 def create_list_of_urls(truncated_html_source_lines):
-    """Creates and returns a list of urls by piecing together .rain filenames, available in the HTML, with known url prefix. Also removes some erroneuos/retired data from the list."""
+    """Creates and returns a list of urls by piecing together .rain filenames,
+    available in the HTML, with known url prefix. Removes two erroneuos/retired
+    locations from list."""
     url_prefix = 'http://or.water.usgs.gov/non-usgs/bes/'
     url_search_string = '.rain'
-    lines_with_matching_url = [s for s in truncated_html_source_lines if url_search_string in s]
+    lines_with_matching_url = [
+        s for s in truncated_html_source_lines if url_search_string in s
+    ]
     url_raw = [line[26:] for line in lines_with_matching_url]
     url_list = [line[:-22] for line in url_raw]
     url_list.pop(url_list.index('rover.rain'))
@@ -48,24 +60,41 @@ def create_list_of_urls(truncated_html_source_lines):
     return completed_list_of_urls
 
 def create_list_of_locations(truncated_html_source_lines):
-    """Creates a list of rain gauge location names by locating a specific string 'Rain Gage<br>' inside the HTML. A list lines is created from the matching truncated_html_source_lines. The lines are then cleaned of uneeded characters and returned into a location_list"""
+    """Creates a list of rain gauge location names by locating a specific
+    string 'Rain Gage<br>' inside the HTML. A list lines is created from the
+    matching truncated_html_source_lines. The lines are then cleaned of uneeded
+    characters and returned into a location_list"""
     location_search_string = 'Rain Gage<br>'
-    lines_with_rain_gauge_address = [s for s in truncated_html_source_lines if location_search_string in s]
+    lines_with_rain_gauge_address = [
+        s for s in truncated_html_source_lines if location_search_string in s
+    ]
     location_raw = [line[4:] for line in lines_with_rain_gauge_address]
     location_list = [line for line in location_raw]
-    location_list = [i.split('Rain Gage<br>', 1)[0] for i in location_list] #cuts the rest of the junk out of each line
+    location_list = [
+        i.split('Rain Gage<br>', 1)[0] for i in location_list
+    ] #cuts the rest of the junk out of each line
     return location_list
 
 def create_list_of_station_numbers(truncated_html_source_lines):
-    """Finds the index postions of the lines in the html source line list which contain the matching partial string and increases the index pos by 1 to reach the line that contains station id"""
-    station_number_indices = [(i+1) for i, s in enumerate(truncated_html_source_lines) if 'Rain Gage<br>' in s]
-    station_number_lines = [truncated_html_source_lines[i] for i in station_number_indices]
+    """Finds the index postions of the lines in the html source line list
+    which contain the matching partial string and increases the index pos
+    by 1 to reach the line that contains station id"""
+    station_number_indices = [(i+1) for i,
+        s in enumerate(truncated_html_source_lines) if 'Rain Gage<br>' in s]
+    station_number_lines = [
+        truncated_html_source_lines[i] for i in station_number_indices]
     station_number_raw = [line[17:] for line in station_number_lines]
     station_number_list = [line[:-6] for line in station_number_raw]
     return station_number_list
 
 def create_rain_gauge_dict():
-    return {z[0]:list(z[1:]) for z in zip(create_list_of_station_numbers(truncated_html_source_lines),create_list_of_locations(truncated_html_source_lines),create_list_of_urls(truncated_html_source_lines))}
+    return {
+    z[0]:list(z[1:]) for z in zip(
+        create_list_of_station_numbers(truncated_html_source_lines),
+        create_list_of_locations(truncated_html_source_lines),
+        create_list_of_urls(truncated_html_source_lines)
+        )
+    }
 
 def get_date_and_total_rain(rain_line_data):
     raw_dates_and_totals = []
@@ -81,14 +110,16 @@ def get_date_and_total_rain(rain_line_data):
         amount = items_in_line[1]
         pair = (date,amount)
         raw_dates_and_totals.append(pair)
-    dates_and_totals = [tuple(s if s != "-" else "0" for s in pair) for pair in raw_dates_and_totals]
+    dates_and_totals = [tuple(
+        s if s != "-" else "0" for s in pair) for pair in raw_dates_and_totals]
     return dates_and_totals
 
 def print_max_rain_date_and_amount(dates_and_totals):
     date,amount = max(dates_and_totals, key=lambda x:int(x[1]))
     inches = (int(amount)/100)
     print('\nCurrently connected to the ' + rain_line_data[0])
-    print('The most rainfall, in a single day, was {} inches on {}.'.format(inches,date))
+    print('The most rainfall, in a single day, was {} inches on {}.'.format(
+        inches,date))
 
 def date_to_year(date):
     year = date[-4:]
@@ -110,14 +141,21 @@ def convert_to_dict(years_and_amounts):
     return(d)
 
 def display_rainiest_year(years_and_amounts):
+    #convert the tuples to a dict
     d = convert_to_dict(years_and_amounts)
-    e = {year:sum(amount) for year,amount in d.items()} #sums together all the values for each key
+    #sum together all the values for each key in the dict
+    e = {year:sum(amount) for year,amount in d.items()}
+    #find the year with max amount of rain
     x = max(e, key=e.get)
+    #find the amount of rain for that year
     y = (e.get(x) * .01)
-    print("{} was the area's wettest year with a total of {} inches of rain.\n".format(x,y))
+    print(
+    "{} was the area's wettest year with a total of {} inches of rain.\n"
+        .format(x,y)
+    )
 
-play = True
-while play:
+run = True
+while run:
     os.system('cls')
     truncated_html_source_lines = parse_index_url_into_lines()[120:]
     rain_gauge_dict = create_rain_gauge_dict()
@@ -127,8 +165,9 @@ while play:
     years_and_amounts = dates_to_years(dates_and_totals)
     convert_to_dict(years_and_amounts)
     display_rainiest_year(years_and_amounts)
-    yayornay = input('Would you like to check another station? [y/n]: ').lower()
+    yayornay = input(
+        'Would you like to check another station? [y/n]: ').lower()
     if yayornay == 'y':
-        play = True
+        run = True
     else:
-        play = False
+        run = False
--- word_count.py
+++ word_count.py
@@ -1,43 +1,55 @@
 # word count program created 3.24
 # Find a book on Project Gutenberg. Download it as a UTF-8 text file.
 #
-# Count how often each unique word is used, then print the most frequent top 10 out with their counts.
-# Count how often each unique pair of words is used, then print the most frequent top 10 out with their counts.
-all_words = [] #creates an empty list called all_words
-words_and_counts={} #creates an empty dict called words_and_counts
-word = ''
+# Count how often each unique word is used, then print the most frequent
+#  top 10 out with their counts.
+# Count how often each unique pair of words is used, then print the most
+#  frequent top 10 out with their counts.
 
+# import urllib.request
+# def parse_txt_into_list_of_lines():
+#     """Parses the main Oregon usgs rain station index webpage into a list of lines"""
+#     with urllib.request.urlopen('http://www.gutenberg.org/cache/epub/1342/pg1342.txt') as data_file:
+#         book_line_data = [byte_line.decode('utf-8') for byte_line in data_file]
+#     return book_line_data
+
+# def parse_txt_into_list_of_lines():
+#     with open('C:\Users\Matt\codeguild\extras\p_and_p.txt') as p_and_p:
+#         book_line_data = p_and_p.readlines() #creates a list of lines
+#     return book_line_data
 def parse_txt_into_list_of_lines():
-    with open('C:\Users\Matt\codeguild\extras\p_and_p.txt') as p_and_p: #opens and reads p_and_p.txt to memory as var p_and_p
-        book_line_data = p_and_p.readlines() #creates a list of values from the lines in p_and_p called book_line_data
+    with open('C:\\Users\\Fizix\\Desktop\\pg1342.txt') as p_and_p:
+        book_line_data = p_and_p.readlines()
     return book_line_data
+
 def normalize_word(word):
     word = word.strip('.,:;"()$-!?<>[]_').lower()
     return word
-def normalize_words(all_words):
-    all_cleaned_words = []
-    for word in all_words:
-        cleaned_word = normalize_word(word)
-        all_cleaned_words.append(cleaned_word)
+
+def normalize_words():
+    all_cleaned_words = [normalize_word(word) for word in split_lines_into_words(book_line_data)]
     return all_cleaned_words
-#List comprehension of normalize_words [normalize_word(word) for word in all_words]
-def count_word_occurances(normalized_word_list, words_and_counts):
-    """Counts the occurances of each word in the normalized_word_list and creates the dict words_and_counts that contains each word and the # of times it occurs"""
-    for word in normalized_word_list: #analyses the values in the normalized_word_list
+
+def count_word_occurrences():
+    """Counts the occurrences of each word in the normalized_word_list and
+    creates the dict words_and_counts that contains each word and the # of
+    times it occurs"""    #use a collections.Counter instead
+    words_and_counts = {}
+    for word in normalize_words(): #analyses the values
         if word not in words_and_counts:
-            words_and_counts[word] = 1 #adds 1 to the count of word in words_and_counts dict if the word is not found
+            words_and_counts[word] = 1 #adds 1 to the count if the word is not found
         else:
-            words_and_counts[word] += 1 #increases the count by 1 of word if it already exists
+            words_and_counts[word] += 1 #increases the count by 1 if word already exists
     return words_and_counts
-def split_lines_into_words(book_line_data, all_words):
+
+def split_lines_into_words(book_line_data):
+    all_words = []
     for line in book_line_data: #analyses the values in the list book_line_data
-        all_words += line.split() #splits the string apart using default delimiter(spaces) and adds each value(word) split to all_words
+        all_words += line.split() #splits the string apart and adds each value(word) split to all_words
     return all_words
 
 book_line_data = parse_txt_into_list_of_lines()
-all_words = split_lines_into_words(book_line_data, all_words)
-normalized_word_list = normalize_words(all_words)
-words_and_counts = count_word_occurances(normalized_word_list, words_and_counts)
-sorted_by_occurance = sorted(words_and_counts.keys(), key=words_and_counts.get, reverse=True)
-print(sorted_by_occurance[:10])
-print(normalized_word_list)
+all_words = split_lines_into_words(book_line_data)
+words_and_counts = count_word_occurrences()
+sorted_by_occurrence = sorted(words_and_counts.keys(), key=words_and_counts.get, reverse=True)
+print(sorted_by_occurrence[:10])
