994a50cddba864d15ac16bd9734620509656a24e
Matt Voelpel <matvop@gmail.com>
2016-04-03 12:56:20 -0700

    atm update

--- atm.py
+++ atm.py
@@ -20,10 +20,11 @@
 # Allow the user to open more than one account.
 # Let them perform all of the above operations by account number.
 import os
-class Account:
-    def setup(self, name, balance):
-        """Return a Customer object whose name is *name* and starting
+class User:
+    def setup(self, account_num, name, balance):
+        """Return a User object whose name is *name* and starting
         balance is *balance*."""
+        self.account_num = account_num
         self.name = name
         self.balance = balance
 
@@ -43,43 +44,48 @@ class Account:
         print('Your current balance is: {}'.format(self.balance))
         return self.balance
 
-os.system('cls')
-user_list = []
-user_list.append(int(input('Please enter your 3 digit account number: ')))
-user_list[0] = Account()
-user_list[0].setup('Matt', 0)
-banking = 'y'
-"""create a dict for storage of user's name, balance, and account#. This
-information should be saved to an external txt."""
-while banking.lower() == 'y':
-    os.system('cls')
-    print(
-        'Welcome {}, would you like to withdraw or deposit funds?'
-            .format(user_list[0].name)
-    )
-    action = int(input("""
-    1 - Withdraw
-    2 - Deposit
+def check_system_for_account():
+    """create a dict for storage of user's name, balance, and account#. This
+    information should be saved to an external txt."""
+    account_dict = {a123:['Matt', 0], a234:['John', 0], a345:['Mary', 0]}
+    me.account_num = input(
+        'Please enter your 4 digit account number(ex:a132): ')
+    # for k in account_dict:
+    #     print(k)
+        # if me.account_num == k:
+        #     me.setup(me.account_num,
+        #     account_dict[me.account_num][0],
+        #     account_dict[me.account_num][1])
+        # else:
+        #     # enter name and have system generate a new account# based on
+        #     # a-z100-999  with blanace 0
+        #     print('create new account here :)')
+        #     break
 
-    >"""))
-    if action == 1:
-        user_list[0].withdraw(
-            int(
-                input(
-                    'Please enter amount you wish to withdraw: '
-                )
-            )
-        )
-        banking = input('would you like to make another transaction? [y/n]: ')
-    if action == 2:
-        user_list[0].deposit(
-            int(
-                input(
-                    'Please enter amount you wish to deposit: '
-                )
-            )
-        )
-        banking = input('would you like to make another transaction? [y/n]: ')
-    else:
-        print('Have a nice day! Goodbye.')
-        banking = 'n'
+# os.system('cls')
+# me = User()
+# banking = 'y'
+# while banking.lower() == 'y':
+#     os.system('cls')
+#     check_system_for_account()
+#     print(
+#         'Welcome {}, would you like to withdraw or deposit funds?'
+#             .format(me.name)
+#     )
+#     action = int(input("""
+#     1 - Withdraw
+#     2 - Deposit
+#     3 - Check Balance
+#
+#     >"""))
+#     if action == 1:
+#         me.withdraw(int(input('Please enter amount you wish to withdraw: ')))
+#         banking = input('would you like to make another transaction? [y/n]: ')
+#     if action == 2:
+#         me.deposit(int(input('Please enter amount you wish to deposit: ')))
+#         banking = input('would you like to make another transaction? [y/n]: ')
+#     if action == 3:
+#         print(me.balance())
+#     else:
+#         print('Have a nice day! Goodbye.')
+#         banking = 'n'
