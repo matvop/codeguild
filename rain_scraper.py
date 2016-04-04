25797c17805f52207d4bcb5a7777d02bf25a3030
Matt Voelpel <matvop@gmail.com>
2016-04-03 19:02:00 -0700

    updates

--- rain_scraper.py
+++ rain_scraper.py
@@ -24,85 +24,12 @@ def parse_index_url():
 
 def parse_rain_file_url():
     """Parses the user selected rain station data into a list of lines"""
-    with urllib.request.urlopen(get_url()) as rain_file:
+    url = gauge_dict[select_rain_gauge()][1]
+    with urllib.request.urlopen(url) as rain_file:
         rain_lines = [
             byte_line.decode('utf-8') for byte_line in rain_file
         ]
     return rain_lines
-
-
-def get_url():
-    """Retrieves the .rain file url by using the users station_num
-    input in select_rain_gauge()"""
-    url = gauge_dict[select_rain_gauge()][1]
-    return url
-
-
-def select_rain_gauge():
-    """Provides the user with a table of available rain stations by their
-    id#, location, and .rain file url. Prompts user for station id#"""
-    print('\n{:<15} {:<40} {:<40}'.format('Station id#:', 'Location:',
-                                          'URL:\n'))
-    for k, v in gauge_dict.items():
-        location, url = v
-        print('{:<15} {:<40} {:<40}'.format(k, location, url))
-    print('')
-    return input("""Rain level data is available for the listed locations.
-
-Please enter station id#: """)
-
-
-# def create_urls(html_source_lines):
-#     """Creates and returns a list of urls by piecing together .rain file
-#     names, available in the HTML, with known url prefix. Removes two
-#     erroneous/retired locations from list."""
-#     url_prefix = 'http://or.water.usgs.gov/non-usgs/bes/'
-#     url_search_string = '.rain'
-#     lines_with_matching_url = [
-#             line for line in html_source_lines
-#             if url_search_string in line
-#     ]
-#     url_raw = [line[26:] for line in lines_with_matching_url]
-#     url_list = [line[:-22] for line in url_raw]
-#     url_list.pop(url_list.index('rover.rain'))
-#     url_list.pop(url_list.index('swan_island.rain'))
-#     completed_list_of_urls = [url_prefix + url for url in url_list]
-#     return completed_list_of_urls
-
-
-# def create_locations(html_source_lines):
-#     """Creates a list of rain gauge location names by locating a specific
-#     string 'Rain Gage<br>' inside the HTML. A list of lines is created from
-#     the matching html_source_lines. The lines are then cleaned of extra
-#     characters and returned into a location_list"""
-#     location_search_string = 'Rain Gage<br>'
-#     lines_with_rain_gauge_address = [
-#         line for line in html_source_lines
-#         if location_search_string in line
-#     ]
-#     location_raw = [line[4:] for line in lines_with_rain_gauge_address]
-#     location_list = [line for line in location_raw]
-#     # cuts the rest of the junk out of each line
-#     location_list = [
-#         i.split('Rain Gage<br>', 1)[0] for i in location_list
-#     ]
-#     return location_list
-
-
-# def create_station_numbers(html_source_lines):
-#     """Finds the index positions of the lines in the html source line list
-#     which contain the matching partial string and increases the index pos
-#     by 1 to reach the line that contains station id"""
-#     station_number_indices = [
-#         (i+1) for i, s in enumerate(
-#             html_source_lines) if 'Rain Gage<br>' in s
-#     ]
-#     station_number_lines = [
-#         html_source_lines[i] for i in station_number_indices
-#     ]
-#     station_number_raw = [line[17:] for line in station_number_lines]
-#     station_number_list = [line[:-6] for line in station_number_raw]
-#     return station_number_list
 
 
 def create_rain_gauge_dict():
@@ -157,6 +84,20 @@ def create_rain_gauge_dict():
     }
 
 
+def select_rain_gauge():
+    """Provides the user with a table of available rain stations by their
+    id#, location, and .rain file url. Prompts user for station id#"""
+    print('\n{:<15} {:<40} {:<40}'.format('Station id#:', 'Location:',
+                                          'URL:\n'))
+    for k, v in gauge_dict.items():
+        location, url = v
+        print('{:<15} {:<40} {:<40}'.format(k, location, url))
+    print('')
+    return input("""Rain level data is available for the listed locations.
+
+Please enter station id#: """)
+
+
 def get_date_and_total_rain():
     rain_line_data = parse_rain_file_url()
     print('\nCurrently connected to the ' + rain_line_data[0])
@@ -179,15 +120,10 @@ def get_date_and_total_rain():
     return dates_and_total_rain
 
 
-def date_to_year(date):
-    year = date[-4:]
-    return year
-
-
 def date_amount_to_year_amount(date_and_amount):
     """brings in the date_and_amount tuple and returns only the year and """
     amount = date_and_amount[1]
-    year = date_to_year(date_and_amount[0])
+    year = date_and_amount[0][-4:]
     return (year, amount)
 
 
