c456676a7cb0735563799275f0f9a1239905d111
Matt Voelpel <matvop@gmail.com>
2016-04-08 09:39:46 -0700

    updates

--- blackjackhand.py
+++ blackjackhand.py
@@ -16,69 +16,85 @@
 import random
 
 
-class Card:
-    names = ['A', '2', '3', '4', '5', '6', '7',
-             '8', '9', '10', 'J', 'Q', 'K']
-    suits = ['C', 'D', 'H', 'S']
-
-    def __init__(self, name, suit):
-        self.name = name
-        self.suit = suit
+class Card(object):
+    def __init__(self, names, suits):
+        self.names = ['A', '2', '3', '4', '5', '6', '7',
+                      '8', '9', '10', 'J', 'Q', 'K']
+        self.suits = ['C', 'D', 'H', 'S']
 
     def __repr__(self):
-        card_string = 'Card({}{})'.format(self.name, self.suit)
-        return card_string
+        reply = '{}{}'.format(self.names, self.suits)
+        return reply
 
-    def generate_rand_card(self):
-        card = Card(random.choice(Card.names), random.choice(Card.suits))
+    def generate_card(self):
+        card = Card(random.choice(self.names), random.choice(self.suits))
         return card
 
-class Hand:
-    def __init__(self, cards):
-        self.cards = cards
+
+class Hand(object):
+    def __init__(self):
+        hand_of_cards = self.hand_of_cards
+
+    hand_of_cards = []
 
     def __repr__(self):
-       return 'Hand({})'.format(self.cards)
+        return 'Hand({})'.format(self.hand_of_cards)
 
-    def add_card(self, card):
-        self.cards.append(Card.generate_rand_card(self))
-        print('Added {} to your hand'.format(self.cards[-1]))
-        # this is where to add option to remove the card if it matches another card_string
-        # for card in self.cards:
+
+    def add_card(self):
+        self.hand_of_cards.append(Card.generate_card(self))
+        print('Added {} to your hand'.format(self.hand_of_cards[-1]))
+        # this is where to add option to remove the card if it matches another
+        # card_string
+        # for card in self.hand_of_cards:
         #     if card == card:
         #         print('this actually works?!')
 
-    def score(self, player_hand):
+    def score(self, split_hand, the_cards):
         points = 0
-        for card in player_hand:
-            if card[0] in Card.names[-3:]:
+        for each in split_hand:
+            if each[0] not in the_cards.card_names[0:13]:
+                print("Not sure what {0} is, disregarding.".format(each))
+            elif each[0] in the_cards.card_names[10:13]:
                 points += 10
-            if card[0] in Card.names[1:10]:
-                points += int(card[0])
-            if card[0:2] == '10':
-                points += int(card[0:2])
-            if card[0] == 'A':
-                points += 1
-        for card in player_hand:
-            for s in card:
-                if 'A' in s and points + 11 <= 21:
+            elif each[0] in the_cards.card_names[1:10]:
+                points += int(each[0])
+            elif each[0] == the_cards.card_names[0]:
+                if points + 11 > 21:
+                    points += 1
+                elif points + 11 <= 21:
                     points += 10
-        return 'You scored {} points!'.format(points)
+        return points
+
+    # def score(self):
+    #     points = 0
+    #     for card in self.hand_of_cards:
+    #         for s in str(card):
+    #             if s in Card.names[-3:]:
+    #                 points += 10
+    #             if s in Card.names[1:10]:
+    #                 points += int(s[0])
+    #             if '10' == s:
+    #                 points += int(s[0:2])
+    #             if 'A' == s:
+    #                 points += 1
+    #     for card in self.hand_of_cards:
+    #         for s in str(card):
+    #             if 'A' in s and points + 11 <= 21:
+    #                 points += 10
+    #     return 'You scored {} points!'.format(points)
 
 
-# card = [random.choice(Card.names), random.choice(Card.suits)]
-
-my_card = [Card(random.choice(Card.names), random.choice(Card.suits))]
+my_card = Card()
 print(my_card)
-my_hand = Hand(my_card)
+my_hand = Hand()
 print(my_hand)
-my_hand.add_card(my_card)
-print(my_hand)
-my_hand.score(my_hand)
-print(my_score)
-
-custom_hand = print(input(str("Please enter a hand to be scored(i.e. 'k q' or 'kd qh'): ")).upper().split())
-print(custom_hand)
-my_hand = Hand(custom_hand)
-print(my_hand)
-print(Hand.score(my_hand))
+my_card.generate_card()
+# print(my_hand)
+# print(my_hand.score())
+# custom_hand = print(input(str("Please enter a hand to be scored(i.e. 'k q' or 'kd qh'): ")).upper().split())
+# print(custom_hand)
+# my_card = (custom_hand[0])
+# print(my_card)
+# my_hand = Hand(custom_hand)
+# print(my_hand.score())
