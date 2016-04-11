f47924290d91fb434256e8ccc1a6bced9c9debaa
Matt Voelpel <matvop@gmail.com>
2016-04-11 15:57:26 -0700

    working - now on to classes

--- connectfour.py
+++ connectfour.py
@@ -23,46 +23,32 @@ class Token(object):
     def __repr__(self):
         pass
 
-    # def add_r_token_to_board():
-    #
-    # def add_y_token_to_board():
 
+# move_list = [4, 3, 5, 6, 4, 4, 5, 3, 6, 2, 7, 7, 3, 7, 4, 5, 6, 5]
+#
+# the_board =[
+# [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0],
+# [0,0,0,0,0,0,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,0]
+# ]
+# def make_the_board():
+#     the_board = []
+#     for row_index in range(6):
+#         row = []
+#         for col_index in range(7):
+#             row.append(0)
+#         the_board.append(row)
+#     return the_board
 
 
 def open_account_database():
     with open('4-moves.txt') as data_file:
-        database = data_file.read()
+        database = data_file.read().split()
     return database
 
 
-
-move_list = [4, 3, 5, 6, 4, 4, 5, 3, 6, 2, 7, 7, 3, 7, 4, 5, 6, 5]
-
-# the_board =[
-# [0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],
-# [0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]
-# ]
-
 def make_the_board():
-    rows = []
-    the_board = []
-    for i in range(7):
-        rows.append(0)
-    for columns in range(6):
-        the_board.append(rows)
+    the_board = [[0 for i in range(7)] for i in range(6)]
     return the_board
-
-# move_database = open_account_database()
-
-def add_token(the_board, move_list):
-    for index, move in enumerate(move_list):
-        if index % 2 == 0:
-            print('R Moved')
-            the_board[find_row_index(the_board, move)][find_column(move)] = 'R'
-        else:
-            print('Y Moved')
-            the_board[find_row_index(the_board, move)][find_column(move)] = 'Y'
-    print('WAKKA WAKKA')
 
 
 def find_column(move):
@@ -77,12 +63,33 @@ def find_row_index(the_board, move):
             return index
 
 
+def add_token(the_board, move_list):
+    for index, move in enumerate(move_list):
+        if index % 2 == 0:
+            the_board[find_row_index(the_board, move)][find_column(move)] = 'R'
+        else:
+            the_board[find_row_index(the_board, move)][find_column(move)] = 'Y'
+    return the_board
+
+
+def convert_database_to_list():
+    move_database = open_account_database()
+    move_list = []
+    for i in move_database[1:-1]:
+        move_list.append(int(i))
+    return move_list
+
+
+def print_results(the_board):
+    print(the_board[-1])
+    print(the_board[-2])
+    print(the_board[-3])
+    print(the_board[-4])
+    print(the_board[-5])
+    print(the_board[-6])
+
+
+move_list = convert_database_to_list()
 the_board = make_the_board()
 add_token(the_board, move_list)
-
-print(the_board[-1])
-print(the_board[-2])
-print(the_board[-3])
-print(the_board[-4])
-print(the_board[-5])
-print(the_board[-6])
+print_results(the_board)
