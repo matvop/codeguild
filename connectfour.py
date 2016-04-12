e318bfeacb5ed3ffff4737c7486c6a3cca756a69
Matt Voelpel <matvop@gmail.com>
2016-04-11 20:45:07 -0700

    updates

--- connectfour.py
+++ connectfour.py
@@ -7,21 +7,37 @@
 
 
 class Board(object):
-    def __init__(self):
-        rows = self.rows
-        columns = self.columns
+    def __init__(self, move_list):
+        self.the_board = [[0 for i in range(7)] for i in range(6)]
+        self.move_list = move_list
 
-    def __repr__(self):
-        pass
+    def __repr__(self): #this prints when you print the object i.e. print(my_game)
+        return 'Board:\n{}\n{}\n{}\n{}\n{}\n{}'.format(
+        self.the_board[-1],
+        self.the_board[-2],
+        self.the_board[-3],
+        self.the_board[-4],
+        self.the_board[-5],
+        self.the_board[-6])
 
+    def find_column(self, move):
+        column = move - 1
+        return column
 
-class Token(object):
-    def __init__(self):
-        red_token = self.red_token
-        yellow_token = self.yellow_token
+    def find_row_index(self, the_board, move):
+        column = self.find_column(move)
+        for index, row in enumerate(self.the_board):
+            if row[column] == 0:
+                return index
 
-    def __repr__(self):
-        pass
+    def add_token(self, move_list):
+        for index, move in enumerate(move_list):
+            if index % 2 == 0:
+                self.the_board[self.find_row_index(self.the_board, move)][self.find_column(move)] = 'R'
+            else:
+                self.the_board[self.find_row_index(self.the_board, move)][self.find_column(move)] = 'Y'
+        return self.the_board #once this runs, i think __repr__ will print
+
 
 
 # move_list = [4, 3, 5, 6, 4, 4, 5, 3, 6, 2, 7, 7, 3, 7, 4, 5, 6, 5]
@@ -46,50 +62,17 @@ def open_account_database():
     return database
 
 
-def make_the_board():
-    the_board = [[0 for i in range(7)] for i in range(6)]
-    return the_board
-
-
-def find_column(move):
-    column = move - 1
-    return column
-
-
-def find_row_index(the_board, move):
-    column = find_column(move)
-    for index, row in enumerate(the_board):
-        if row[column] == 0:
-            return index
-
-
-def add_token(the_board, move_list):
-    for index, move in enumerate(move_list):
-        if index % 2 == 0:
-            the_board[find_row_index(the_board, move)][find_column(move)] = 'R'
-        else:
-            the_board[find_row_index(the_board, move)][find_column(move)] = 'Y'
-    return the_board
-
-
-def convert_database_to_list():
+def create_move_list():
     move_database = open_account_database()
     move_list = []
     for i in move_database[1:-1]:
         move_list.append(int(i))
     return move_list
 
+def main():
+    move_list = create_move_list()
+    my_game = Board(move_list)
+    my_game.add_token(move_list)
+    print(my_game)
 
-def print_results(the_board):
-    print(the_board[-1])
-    print(the_board[-2])
-    print(the_board[-3])
-    print(the_board[-4])
-    print(the_board[-5])
-    print(the_board[-6])
-
-
-move_list = convert_database_to_list()
-the_board = make_the_board()
-add_token(the_board, move_list)
-print_results(the_board)
+main()
