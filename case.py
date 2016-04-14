458234ccf816d1305fc9a6f2537132e485c4415b
Matt Voelpel <matvop@gmail.com>
2016-04-14 16:13:39 -0700

    updated case.py

--- case.py
+++ case.py
@@ -12,7 +12,6 @@ import re
 
 
 def snake_to_camel():
-    print('Your word: ' + prompt_for_string())
     new_list = prompt_for_string().split('_')
     camel_case = ''.join([i[0].upper() + i[1:] for i in new_list])
     return camel_case
@@ -33,32 +32,44 @@ def camel_to_snake():
     return snake_case.lower()
 
 
-# def convert_snake():
-# def convert_camel():
-# def convert_kebab():
-# def convert_constant():
+def convert_snake(orig_string):
+    print('CamelCase: ' + ''.join([i[0].upper() + i[1:] for i in orig_string.split('_')])) #CamelCase
+    print('kebab-case: ' + '-'.join([i for i in orig_string.split('_')])) #kebab-case
+    print('CONSTANT_CASE: ' + '_'.join([i for i in orig_string.split('_')]).upper()) #CONSTANT_CASE
 
 
-def detect_orig_case():
-    list_of_func = ['snake_case', 'CamelCase', 'kebab-case', 'CONSTANT_CASE']
-    orig_string = prompt_for_string()
-    if orig_string == orig_string.upper():
-        print(list_of_func[3])
-        convert_constant()
-    if '-' in orig_string:
-        print(list_of_func[2])
-        convert_kebab()
-        return orig_string
-    if orig_string in re.split('_|-|;|, ', orig_string):
-        print(list_of_func[1])
-        convert_camel()
-    if orig_string.split('_') != re.split('-|;|,| ', orig_string):
-        print(list_of_func[0])
-        convert_snake()
-    else:
-        print('Not Found')
+def convert_camel(orig_string):
+    print('snake_case: ' + '_'.join([i for i in re.findall('[A-Z][^A-Z]*', orig_string)]).lower()) #snake_case
+    print('kebab-case: ' + '-'.join([i for i in re.findall('[A-Z][^A-Z]*', orig_string)]).lower()) #kebab-case
+    print('CONSTANT_CASE: ' + '_'.join([i for i in re.findall('[A-Z][^A-Z]*', orig_string)]).upper()) #CONSTANT_CASE
+
+
+def convert_kebab(orig_string):
+    print('CamelCase: ' + ''.join([i[0].upper() + i[1:] for i in orig_string.split('-')])) # CamelCase
+    print('snake_case: ' + '_'.join([i for i in orig_string.split('-')])) # snake_case
+    print('CONSTANT_CASE: ' + '_'.join([i for i in orig_string.split('-')]).upper()) # CONSTANT_CASE
+
+
+def convert_constant(orig_string):
+    print('CamelCase: ' + ''.join([i[0].upper() + i[1:] for i in orig_string.lower().split('_')])) # CamelCase
+    print('kebab-case: ' + '-'.join([i for i in orig_string.lower().split('_')])) # kebab
+    print('snake_case: ' + orig_string.lower()) # snake
+
 
 def prompt_for_string():
     return input('Please enter a string to be converted\n> ')
 
-detect_orig_case()
+
+def main():
+    orig_string = prompt_for_string()
+    if orig_string == orig_string.upper():
+        return convert_constant(orig_string)
+    if '-' in orig_string:
+        return convert_kebab(orig_string)
+    if orig_string in re.split('_|-|;|, ', orig_string):
+        return convert_camel(orig_string)
+    if orig_string == orig_string.lower():
+        return convert_snake(orig_string)
+
+
+main()
