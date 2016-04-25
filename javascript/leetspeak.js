dfa24f37162b2efc0dbaf892f62bc2580703ae47
Matt <matvop@gmail.com>
2016-04-25 10:48:27 -0700

    updates

---
+++ javascript/leetspeak.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var convertWordToLeetSpeak = function(originalWord) {
+    var originalCharToLeetChar = {
+        "o":"0",
+        "l":"1",
+        "e":"3",
+        "a":"4",
+        "t":"7",
+        "s":"Z"
+    };
+
+    for (var originalChar in originalCharToLeetChar) {
+        var leetChar = originalCharToLeetChar[originalChar];
+        originalWord = originalWord.replace(originalChar, leetChar);
+    }
+
+
+    return originalWord;
+};
+
+console.log(convertWordToLeetSpeak("hello"));
