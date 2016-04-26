967f6646d7dc9f39ae96a5e2135de22b21f5651b
Matt <matvop@gmail.com>
2016-04-26 11:03:07 -0700

    updates

--- javascript/sumpairs.js
+++ javascript/sumpairs.js
@@ -1,1 +1,29 @@
+// Practice: Sum Pairs
+// Write a function named find_sum_pairs. It takes two arguments: a list of ints to search, and a sum to find.
+//
+// Go through the search list and find all pairs of numbers that would add together to the sum.
+//
+// Example output:
+//
+// >>> find_sum_pairs([-1, 0, 1, 2], 3)
+// [[1, 2]]
+// >>> find_sum_pairs([-1, 0, 1, 2], 1)
+// [[-1, 2], [0, 1]]
+// >>> find_sum_pairs([2, -1, 2], 1)
+// [[2, -1], [-1, 2]]
+// >>> find_sum_pairs([-1, 1, 2, 2], 3)
+// [[1, 2], [1, 2]]
+
 "use strict";
+
+var testList = [-1, 1, 2, 0, 3];
+var testSum = 2;
+
+var find_sum_pairs = function(intList, sum) {
+    for (var int in intList) {
+        
+    }
+};
+
+
+console.log(find_sum_pairs(testList, testSum));
