0cd67c33c656f4f75deb1121abce91102c879685
Matt <matvop@gmail.com>
2016-05-03 09:38:58 -0700

    updates

--- javascript/album.js
+++ javascript/album.js
@@ -44,18 +44,18 @@ function registerGlobalEventHandlers() {
         updateTileCount();
         $('form').children('#url-input').val('');
         $('.image-popup-no-margins').magnificPopup({
-        		type: 'image',
-        		closeOnContentClick: true,
-        		closeBtnInside: false,
-        		fixedContentPos: true,
-        		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-        		image: {
-        			verticalFit: true
-        		},
-        		zoom: {
-        			enabled: true,
-        			duration: 1000 // don't foget to change the duration also in CSS
-        		}
+    		type: 'image',
+    		closeOnContentClick: true,
+    		closeBtnInside: false,
+    		fixedContentPos: true,
+    		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+    		image: {
+    			verticalFit: true
+    		},
+    		zoom: {
+    			enabled: true,
+    			duration: 1000 // don't foget to change the duration also in CSS
+    		}
         });
     });
 }
